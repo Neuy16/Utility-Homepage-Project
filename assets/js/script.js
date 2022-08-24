aa979914322283b35fdb64c84914f36032c14ae1
Jake Z <108641152+Jzander@users.noreply.github.com>
2022-08-23 20:19:58 -0400

    added moment.js and yeezy quotes

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,10 +18,25 @@
 
 
 // Jacob Z's Section
+/* Fetching Kanye's wisdom */
+async function getYeezy() {
+    const response = await fetch('https://api.kanye.rest');
+    const data = await response.json();
+    
+    console.log(data);
 
+    
+}
+getYeezy();
+/* Tellng the time */
+function time() {
+    var currentDay = moment().format('dddd MMMM Do YYYY');
+    var currentTime = moment().format('hh:mm:ss');
 
-
-
+    console.log(currentDay);
+    console.log(currentTime);
+}
+time();
 
 
 
