4eb79916ebd1a58f738f624917180a5b964c181d
juicyjake12 <108505695+juicyjake12@users.noreply.github.com>
2022-08-25 19:02:45 -0400

    api added

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,9 +8,9 @@
 
 
 // Jacob H's Section
-
-
-
+fetch("http://api.mediastack.com/v1/news?access_key=e88991d67556e60781314a23d73242b8")
+    .then((response) => response.json())
+    .then((data) => console.log(data));
 
 
 
