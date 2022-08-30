c5d7d699ea16f44debc75dae1d48bb6b9bdb7402
juicyjake12 <108505695+juicyjake12@users.noreply.github.com>
2022-08-30 18:51:51 -0400

    array to page

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,28 +3,28 @@
 
 //begin weather app
 let weather = {
-    apiKey: "ce799edeec116539e35783770070d97d",
-    fetchWeather: function (city) {
-        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + this.apiKey)
-        .then((response) => response.json())
-        .then((data) => this.displayWeather(data));
-    },
-    displayWeather: function (data) {
-        const { name } = data;
-        const { temp, humidity } = data.main;
-        const { icon, description } = data.weather[0];
-        const { speed } = data.wind;
-        document.querySelector("#icon").src = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
-        document.querySelector("#description").innerText = description;
-        document.querySelector("#temp").innerText = temp + "°f";
-    },
-    search: function () {
-        this.fetchWeather(document.querySelector(".searchBar").value);
-    },
+  apiKey: "ce799edeec116539e35783770070d97d",
+  fetchWeather: function (city) {
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + this.apiKey)
+      .then((response) => response.json())
+      .then((data) => this.displayWeather(data));
+  },
+  displayWeather: function (data) {
+    const { name } = data;
+    const { temp, humidity } = data.main;
+    const { icon, description } = data.weather[0];
+    const { speed } = data.wind;
+    document.querySelector("#icon").src = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+    document.querySelector("#description").innerText = description;
+    document.querySelector("#temp").innerText = temp + "°f";
+  },
+  search: function () {
+    this.fetchWeather(document.querySelector(".searchBar").value);
+  },
 };
 
 document.querySelector(".search button").addEventListener("click", function () {
-    weather.search();
+  weather.search();
 });
 
 weather.fetchWeather("Detroit");
@@ -33,13 +33,13 @@ weather.fetchWeather("Detroit");
 
 //begin web link button section
 function newbutton() {
-    var div = document.querySelector('.presetLinks');
-    var input = document.querySelector('#getUrlForButton');
+  var div = document.querySelector('.presetLinks');
+  var input = document.querySelector('#getUrlForButton');
 
-    var dom = '<a href="https://' + input.value + '.com' + '" target="_blank">';
-    dom += '<input type="button" class="rounded border-10 border-sky-300 bg-sky-300" value=" ' + input.value  + ' "></a>';
+  var dom = '<a href="https://' + input.value + '.com' + '" target="_blank">';
+  dom += '<input type="button" class="rounded border-10 border-sky-300 bg-sky-300" value=" ' + input.value + ' "></a>';
 
-    div.innerHTML += dom;
+  div.innerHTML += dom;
 }
 //end web link button section
 
@@ -49,20 +49,20 @@ function newbutton() {
 var modal1 = document.getElementById("modal1");
 var modalBtn1 = document.getElementById("modalBtn1");
 var closeBtn1 = document.getElementsByClassName("closeBtn1")[0];
- 
+
 modalBtn1.addEventListener("click", openModal1);
 closeBtn1.addEventListener("click", closeModal1);
 window.addEventListener("click", clickOutsideModal1);
 
-function openModal1 () {
+function openModal1() {
   modal1.style.display = "block";
 }
 
-function closeModal1 () {
+function closeModal1() {
   modal1.style.display = "none";
 }
 
-function clickOutsideModal1 (event1) {
+function clickOutsideModal1(event1) {
   if (event1.target == modal1) {
     modal1.style.display = "none";
   }
@@ -72,20 +72,20 @@ function clickOutsideModal1 (event1) {
 var modal2 = document.getElementById("modal2");
 var modalBtn2 = document.getElementById("modalBtn2");
 var closeBtn2 = document.getElementsByClassName("closeBtn2")[0];
- 
+
 modalBtn2.addEventListener("click", openModal2);
 closeBtn2.addEventListener("click", closeModal2);
 window.addEventListener("click", clickOutsideModal2);
 
-function openModal2 () {
+function openModal2() {
   modal2.style.display = "block";
 }
 
-function closeModal2 () {
+function closeModal2() {
   modal2.style.display = "none";
 }
 
-function clickOutsideModal2 (event2) {
+function clickOutsideModal2(event2) {
   if (event2.target == modal2) {
     modal2.style.display = "none";
   }
@@ -95,20 +95,20 @@ function clickOutsideModal2 (event2) {
 var modal3 = document.getElementById("modal3");
 var modalBtn3 = document.getElementById("modalBtn3");
 var closeBtn3 = document.getElementsByClassName("closeBtn3")[0];
- 
+
 modalBtn3.addEventListener("click", openModal3);
 closeBtn3.addEventListener("click", closeModal3);
 window.addEventListener("click", clickOutsideModal3);
 
-function openModal3 () {
+function openModal3() {
   modal3.style.display = "block";
 }
 
-function closeModal3 () {
+function closeModal3() {
   modal3.style.display = "none";
 }
 
-function clickOutsideModal3 (event3) {
+function clickOutsideModal3(event3) {
   if (event3.target == modal3) {
     modal3.style.display = "none";
   }
@@ -118,20 +118,20 @@ function clickOutsideModal3 (event3) {
 var modal4 = document.getElementById("modal4");
 var modalBtn4 = document.getElementById("modalBtn4");
 var closeBtn4 = document.getElementsByClassName("closeBtn4")[0];
- 
+
 modalBtn4.addEventListener("click", openModal4);
 closeBtn4.addEventListener("click", closeModal4);
 window.addEventListener("click", clickOutsideModal4);
 
-function openModal4 () {
+function openModal4() {
   modal4.style.display = "block";
 }
 
-function closeModal4 () {
+function closeModal4() {
   modal4.style.display = "none";
 }
 
-function clickOutsideModal4 (event4) {
+function clickOutsideModal4(event4) {
   if (event4.target == modal4) {
     modal4.style.display = "none";
   }
@@ -140,31 +140,41 @@ function clickOutsideModal4 (event4) {
 
 
 // Jacob H's Section
-fetch("http://api.mediastack.com/v1/news?access_key=e88991d67556e60781314a23d73242b8")
-    .then((response) => response.json())
-    .then((data) => console.log(data));
+
+
+fetch("https://api.goperigon.com/v1/all?apiKey=eed9a585-f20e-4441-bcf0-ea92298d2788")
+.then(response => response.json())
+.then(data => {
+  let Array
+  for(let i=0; i<data.articles.length; i++){
+    Array = data.articles[i]
+    console.log(Array);
+  }
+newsarticles = Array;
+console.log(newsarticles)
+});
 
 
 
 
-
+  
 
 // Jacob Z's Section
 /* Fetching Kanye's wisdom */
 async function getYeezy() {
-    const response = await fetch('https://api.kanye.rest');
-    const data = await response.json();
-    document.getElementById('quote').innerHTML = "''" + data.quote + "''" + '  -Kanye West'; 
+  const response = await fetch('https://api.kanye.rest');
+  const data = await response.json();
+  document.getElementById('quote').innerHTML = "''" + data.quote + "''" + '  -Kanye West';
 
 
 }
 getYeezy();
 /* Tellng the time */
 function time() {
-    var currentDay = moment().format('dddd MMMM Do YYYY');
-    var currentTime = moment().format('hh:mm');
-    document.getElementById('time').innerHTML = currentTime;
-    document.getElementById('date').innerHTML = currentDay;
+  var currentDay = moment().format('dddd MMMM Do YYYY');
+  var currentTime = moment().format('hh:mm');
+  document.getElementById('time').innerHTML = currentTime;
+  document.getElementById('date').innerHTML = currentDay;
 
 }
 setInterval(time, 1000);
